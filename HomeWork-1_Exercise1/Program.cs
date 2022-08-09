c0b7ddda3a4b9da1a12115b9effdb888fcaaae12
Melnikov <artem.melnikov.ara@gmail.com>
2022-08-09 18:34:58 +0300

    1st exercise

---
+++ HomeWork-1_Exercise1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int max = number1;
+if (number2 > max)
+{
+    max = number2;
+}
+Console.WriteLine($"Максимальное введёное число равно = {max}");
