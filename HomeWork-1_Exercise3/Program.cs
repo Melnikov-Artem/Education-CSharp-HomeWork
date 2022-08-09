1ba81468c542d4a2efd5fca40d6c6d774b0fe7d8
Melnikov <artem.melnikov.ara@gmail.com>
2022-08-09 18:56:47 +0300

    3th exercise

---
+++ HomeWork-1_Exercise3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+string result = "нет";
+if (number % 2 == 0)
+{
+    result = "да";
+}
+Console.WriteLine(result);
