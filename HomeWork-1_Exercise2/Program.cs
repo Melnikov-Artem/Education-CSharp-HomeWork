0fc40426d00284cd63892733d8cfb67f2c2ea023
Melnikov <artem.melnikov.ara@gmail.com>
2022-08-09 19:26:19 +0300

    4th exercise

--- HomeWork-1_Exercise2/Program.cs
+++ HomeWork-1_Exercise2/Program.cs
@@ -5,6 +5,6 @@ int number2 = int.Parse(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int number3 = int.Parse(Console.ReadLine());
 int max = number1;
-if (number2 > max){max = number2;}
-if (number3 > max){max = number3;}
+if (number2 > max) { max = number2; }
+if (number3 > max) { max = number3; }
 Console.WriteLine($"Максимальное введёное число равно = {max}");
---
+++ HomeWork-1_Exercise4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int index = 1;
+string count = "";
+while (N>(index-1))
+{
+    if (index % 2 == 0)
+    {
+        count = count + ($"{index}, ");
+    }
+    index++;
+}
+Console.WriteLine(count);
